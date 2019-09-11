300c741b32bb91898697980d027ce2efdfc4f0e1
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-11 15:21:52 +0100

    tested navigation and submit buttons on selenium testing site

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -33,23 +33,81 @@ public class GoogleSeleniumTest {
     public void searchTest() throws InterruptedException {
         driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
         Thread.sleep(1000);
-        WebElement searchField = driver.findElement(By.name("q"));
+
+        WebElement pageDown = driver.findElementByTagName("body");
+        WebElement pageUp = driver.findElementByTagName("body");
+
+
+
+        WebElement searchField = driver.findElementById("user-message");
         assertTrue(searchField.isDisplayed());
-        searchField.sendKeys("funny cat pictures");
-        Thread.sleep(3000);
-        WebElement submitButton = driver.findElement(By.name("btnK"));
-        submitButton.click();
-        Thread.sleep(3000);
-        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for funny cat");
-        linkToBiggerPicture.click();
-        Thread.sleep(3000);
-        WebElement clickAPicture = driver.findElementById("D-PLcqeLGxB5CM:");
-        clickAPicture.click();
-        Thread.sleep(3000);
-//        WebElement scroll = driver.findElementByName("gsr");
-//        scroll.sendKeys(Keys.chord(Keys.PAGE_DOWN));
-//        Thread.sleep(6000);                 (this doesnt quite work)
+        searchField.sendKeys("KEYBOARD");
+        Thread.sleep(250);
+        WebElement showMessage = driver.findElementByXPath("//*[@id=\"get-input\"]/button");
+        showMessage.click();
+        Thread.sleep(250);
+
+        pageDown.sendKeys(Keys.PAGE_DOWN);
+        Thread.sleep(250);
+
+        WebElement enterA = driver.findElementById("sum1");
+        assertTrue(enterA.isDisplayed());
+        enterA.sendKeys("5");
+        Thread.sleep(250);
+        WebElement enterB = driver.findElementById("sum2");
+        assertTrue(enterB.isDisplayed());
+        enterB.sendKeys("12");
+        Thread.sleep(250);
+        WebElement getTotal = driver.findElementByXPath("//*[@id=\"gettotal\"]/button");
+        getTotal.click();
+        Thread.sleep(250);
+
+        pageUp.sendKeys(Keys.PAGE_UP);
+        Thread.sleep(250);
+
+        WebElement menuInputForms = driver.findElementByXPath("//*[@id=\"treemenu\"]/li/ul/li[1]/a");
+        menuInputForms.click();
+        Thread.sleep(250);
+        WebElement menuRadioButts = driver.findElementByXPath("//*[@id=\"treemenu\"]/li/ul/li[1]/ul/li[3]/a");
+        menuRadioButts.click();
+        Thread.sleep(250);
+
+        WebElement selectM = driver.findElementByName("optradio");
+        selectM.click();
+        Thread.sleep(250);
+        WebElement submitM = driver.findElementByXPath("//*[@id=\"buttoncheck\"]");
+        submitM.click();
+        Thread.sleep(250);
+
+        WebElement selectF = driver.findElementByXPath("//*[@id=\"easycont\"]/div/div[2]/div[2]/div[2]/div[1]/label[2]/input");
+        selectF.click();
+        Thread.sleep(250);
+        WebElement selectAge = driver.findElementByXPath("//*[@id=\"easycont\"]/div/div[2]/div[2]/div[2]/div[2]/label[2]/input");
+        selectAge.click();
+        Thread.sleep(250);
+        WebElement submitVals = driver.findElementByXPath("//*[@id=\"easycont\"]/div/div[2]/div[2]/div[2]/button");
+        submitVals.click();
+        Thread.sleep(2000);
+
+
+
+
+//        pageDown.sendKeys(Keys.PAGE_DOWN);
+//        Thread.sleep(1000);
+
+
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
